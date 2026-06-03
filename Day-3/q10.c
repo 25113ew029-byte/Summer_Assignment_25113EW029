daf19fbd70f5671d4248664fbe7b6cd9d0102ddb
Raunak Gupta <25113ew029@glbitm.ac.in>
2026-06-03 22:02:53 +0530

    q10.c

--- Day-3/q10.c
+++ Day-3/q10.c
@@ -1,33 +1,24 @@
 // Write a program to prime number in a range.
 #include <stdio.h>
-
 int main() {
-    int start, end, i, j, isPrime;
-
-    printf("Enter the starting number: ");
-    scanf("%d", &start);
-
-    printf("Enter the ending number: ");
-    scanf("%d", &end);
-
-    printf("Prime numbers between %d and %d are:\n", start, end);
-
-    for (i = start; i <= end; i++) {
-        if (i < 2)
-            continue;
-
-        isPrime = 1;
-
-        for (j = 2; j <= i / 2; j++) {
-            if (i % j == 0) {
-                isPrime = 0;
-                break;
-            }
-        }
-
-        if (isPrime)
-            printf("%d ", i);
+int start, end, i, j, isPrime;
+printf("Enter the starting number: ");
+scanf("%d", &start);
+ printf("Enter the ending number: ");
+ scanf("%d", &end);
+printf("Prime numbers between %d and %d are:\n", start, end);
+for (i = start; i <= end; i++) {
+ if (i < 2)
+ continue;
+isPrime = 1;
+ for (j = 2; j <= i / 2; j++) {
+if (i % j == 0) {
+isPrime = 0;
+ break;
+ }
+   }
+ if (isPrime)
+ printf("%d ", i);
     }
-
-    return 0;
+ return 0;
 }
