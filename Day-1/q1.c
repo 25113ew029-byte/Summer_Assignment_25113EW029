c6de7aebbddd1df81b10e5aa91634639c284c023
Raunak Gupta <25113ew029@glbitm.ac.in>
2026-06-03 21:14:52 +0530

    q1.c

--- Day-1/q1.c
+++ Day-1/q1.c
@@ -1,15 +1,10 @@
 // write  a program to calculate sum of first N natural numbers.
 #include <stdio.h>
-
 int main() {
     int n, sum;
-
-    printf("Enter a number: ");
+printf("Enter a number: ");
     scanf("%d", &n);
-
-    sum = n * (n + 1) / 2;
-
-    printf("Sum of first %d natural numbers = %d\n", n, sum);
-
-    return 0;
+sum = n * (n + 1) / 2;
+printf("Sum of first %d natural numbers = %d\n", n, sum);
+ return 0;
 }
