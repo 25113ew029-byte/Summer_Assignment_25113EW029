b83083970ab7a0cceff62dc6ee32ac8842d6dda8
Raunak Gupta <25113ew029@glbitm.ac.in>
2026-06-03 21:16:11 +0530

    q2.c

--- Day-1/q2.c
+++ Day-1/q2.c
@@ -1,17 +1,11 @@
 //write a program to print multiplication table of a given number.
 #include <stdio.h>
-
 int main() {
     int num, i;
-
-    // Input from user
     printf("Enter a number: ");
     scanf("%d", &num);
-
-    // Print multiplication table
     for(i = 1; i <= 10; i++) {
         printf("%d x %d = %d\n", num, i, num * i);
     }
-
-    return 0;
+return 0;
 }
