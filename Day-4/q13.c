b7e333b439c206b719f257517cd73e36a9b16411
Raunak Gupta <25113ew029@glbitm.ac.in>
2026-06-03 22:11:53 +0530

    q13.c

--- Day-4/q13.c
+++ Day-4/q13.c
@@ -1,20 +1,15 @@
 // write a program to grnerate fibonacci series.
 #include <stdio.h>
-
 int main() {
-    int n, first = 0, second = 1, next, i;
-
-    printf("Enter the number of terms: ");
-    scanf("%d", &n);
-
-    printf("Fibonacci Series: ");
-
-    for(i = 1; i <= n; i++) {
-        printf("%d ", first);
-        next = first + second;
-        first = second;
-        second = next;
-    }
-
-    return 0;
+ int n, first = 0, second = 1, next, i;
+printf("Enter the number of terms: ");
+scanf("%d", &n);
+printf("Fibonacci Series: ");
+ for(i = 1; i <= n; i++) {
+ printf("%d ", first);
+ next = first + second;
+ first = second;
+second = next;
+  }
+ return 0;
 }
