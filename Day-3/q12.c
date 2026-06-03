84f22cc9ff29c464490b44f531c5c3b437ac57f2
Raunak Gupta <25113ew029@glbitm.ac.in>
2026-06-03 22:09:48 +0530

    q12.c

--- Day-3/q12.c
+++ Day-3/q12.c
@@ -1,25 +1,18 @@
 // Write a program to find LCM of two numbers.
 #include <stdio.h>
-
 int main() {
-    int num1, num2, a, b, gcd, lcm;
-
-    printf("Enter two numbers: ");
-    scanf("%d %d", &num1, &num2);
-
-    a = num1;
-    b = num2;
-
-    while (b != 0) {
-        int temp = b;
-        b = a % b;
-        a = temp;
+ int num1, num2, a, b, gcd, lcm;
+printf("Enter two numbers: ");
+scanf("%d %d", &num1, &num2);
+ a = num1;
+ b = num2;
+ while (b != 0) {
+ int temp = b;
+ b = a % b;
+a = temp;
     }
-
-    gcd = a;
-    lcm = (num1 * num2) / gcd;
-
-    printf("LCM = %d", lcm);
-
-    return 0;
+gcd = a;
+ lcm = (num1 * num2) / gcd;
+ printf("LCM = %d", lcm);
+return 0;
 }
