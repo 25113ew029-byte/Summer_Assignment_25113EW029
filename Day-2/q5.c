86a146c666dcf47cc737421359139921aa184739
Raunak Gupta <25113ew029@glbitm.ac.in>
2026-06-03 21:49:22 +0530

    q5.c

--- Day-2/q5.c
+++ Day-2/q5.c
@@ -1,19 +1,14 @@
 // write a program to find sum of digit of a number
 #include <stdio.h>
-
 int main() {
     int num, digit, sum = 0;
-
-    printf("Enter a number: ");
-    scanf("%d", &num);
-
-    while (num != 0) {
-        digit = num % 10;  // Extract last digit
-        sum = sum + digit; // Add digit to sum
-        num = num / 10;    // Remove last digit
+ printf("Enter a number: ");
+scanf("%d", &num);
+ while (num != 0) {
+        digit = num % 10; 
+        sum = sum + digit; 
+        num = num / 10;   
     }
-
-    printf("Sum of digits = %d\n", sum);
-
-    return 0;
+printf("Sum of digits = %d\n", sum);
+return 0;
 }
