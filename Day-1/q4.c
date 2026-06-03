b38e74b9358663d8c6a3efa9061f611cb33885f5
Raunak Gupta <25113ew029@glbitm.ac.in>
2026-06-03 21:46:54 +0530

    q4.c

--- Day-1/q4.c
+++ Day-1/q4.c
@@ -1,21 +1,13 @@
 // write a program to count digit in a number .
 #include <stdio.h>
-
 int main() {
-    int num, count = 0;
-
-    // Input from user
-    printf("Enter a number: ");
-    scanf("%d", &num);
-
-    // Count digits
-    while(num != 0) {
-        num = num / 10;
-        count++;
-    }
-
-    // Display result
-    printf("Total digits = %d\n", count);
-
-    return 0;
+int num, count = 0;
+printf("Enter a number: ");
+scanf("%d", &num);
+while(num != 0) {
+ num = num / 10;
+count++;
+ }
+ printf("Total digits = %d\n", count);
+return 0;
 }
