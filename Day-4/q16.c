dc42799de0e60beedb375e6c668bdb13c4bfa1f1
Raunak Gupta <25113ew029@glbitm.ac.in>
2026-06-03 22:36:58 +0530

    q16.c

--- Day-4/q16.c
+++ Day-4/q16.c
@@ -1,33 +1,24 @@
 // Write a program to print Armstrong number ina range.
 #include <stdio.h>
-
 int main() {
-    int start, end, num, originalNum, remainder, result;
-
-    printf("Enter start of range: ");
-    scanf("%d", &start);
-
-    printf("Enter end of range: ");
-    scanf("%d", &end);
-
-    printf("Armstrong numbers between %d and %d are:\n", start, end);
-
-    for (num = start; num <= end; num++) {
-        originalNum = num;
-        result = 0;
-
-        while (originalNum != 0) {
-            remainder = originalNum % 10;
-            result += remainder * remainder * remainder; // cube of digit
-            originalNum /= 10;
-        }
-
-        if (result == num) {
-            printf("%d ", num);
-        }
-    }
-
-    printf("\n");
-
-    return 0;
+int start, end, num, originalNum, remainder, result;
+printf("Enter start of range: ");
+scanf("%d", &start);
+ printf("Enter end of range: ");
+scanf("%d", &end);
+printf("Armstrong numbers between %d and %d are:\n", start, end);
+for (num = start; num <= end; num++) {
+ originalNum = num;
+ result = 0;
+while (originalNum != 0) {
+ remainder = originalNum % 10;
+result += remainder * remainder * remainder; 
+ originalNum /= 10;
+     }
+if (result == num) {
+ printf("%d ", num);
+ }
+ }
+ printf("\n");
+ return 0;
 }
