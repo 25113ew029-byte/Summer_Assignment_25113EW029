2ec4de2d2d83dadb0ede6763d62bb83eb8851e92
Raunak Gupta <25113ew029@glbitm.ac.in>
2026-06-03 21:55:39 +0530

    q8.c

--- Day-2/q8.c
+++ Day-2/q8.c
@@ -1,24 +1,18 @@
 // Write a program to click whether a number is palindrome.
 #include <stdio.h>
-
 int main() {
-    int num, originalNum, remainder, reverse = 0;
-
-    printf("Enter a number: ");
-    scanf("%d", &num);
-
-    originalNum = num;
-
-    while (num != 0) {
-        remainder = num % 10;
-        reverse = reverse * 10 + remainder;
-        num = num / 10;
+int num, originalNum, remainder, reverse = 0;
+printf("Enter a number: ");
+scanf("%d", &num);
+originalNum = num;
+while (num != 0) {
+remainder = num % 10;
+reverse = reverse * 10 + remainder;
+num = num / 10;
     }
-
-    if (originalNum == reverse)
-        printf("%d is a palindrome number.\n", originalNum);
-    else
-        printf("%d is not a palindrome number.\n", originalNum);
-
-    return 0;
+if (originalNum == reverse)
+printf("%d is a palindrome number.\n", originalNum);
+ else
+printf("%d is not a palindrome number.\n", originalNum);
+ return 0;
 }
