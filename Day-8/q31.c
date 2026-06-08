43a29479ec3af99c59489fd103db0dcdddd411cf
Raunak Gupta <25113ew029@glbitm.ac.in>
2026-06-08 14:43:33 +0530

    Day-8

---
+++ Day-8/q31.c
@@ -0,0 +1,17 @@
+// write a program to print character triangle.
+// A
+//AB
+//ABC
+//ABCD
+//ABCDE
+#include <stdio.h>
+int main() {
+int i, j;
+for(i = 1; i <= 5; i++) {
+for(j = 0; j < i; j++) {
+printf("%c", 'A' + j);
+}
+printf("\n");
+ }
+return 0;
+}
