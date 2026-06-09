df39f93db7a4c6f72540a67a664f5372b5a63594
Raunak Gupta <25113ew029@glbitm.ac.in>
2026-06-09 21:24:47 +0530

    Day-10 uploaded

---
+++ Day-10/q40.c
@@ -0,0 +1,30 @@
+// Write a program to print character pyramid.
+//     A
+//    ABA
+//   ABCBA
+//  ABCDCBA
+// ABCDEDCBA
+#include <stdio.h>
+int main() {
+int i, j, n;
+char ch;
+printf("Enter the number of rows: ");
+scanf("%d", &n);
+for (i = 1; i <= n; i++) {
+for (j = 1; j <= n - i; j++) {
+printf(" ");
+}
+ch = 'A';
+for (j = 1; j <= i; j++) {
+printf("%c", ch);
+ch++;
+}
+ch -= 2;
+for (j = 1; j < i; j++) {
+printf("%c", ch);
+ch--;
+}
+printf("\n");
+ }
+return 0;
+}
