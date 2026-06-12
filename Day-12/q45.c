9ab0d5d773c3e5953246cddbec9da2a7f039bac7
Raunak Gupta <25113ew029@glbitm.ac.in>
2026-06-12 20:56:12 +0530

    Day-12 uploaded

---
+++ Day-12/q45.c
@@ -0,0 +1,27 @@
+// Write a program to  Write function for palindrome.
+#include <stdio.h>
+int palindrome(int n)
+{
+int original = n, reverse = 0, rem;
+while (n != 0)
+{
+rem = n % 10;
+reverse = reverse * 10 + rem;
+n = n / 10;
+}
+if (original == reverse)
+return 1;
+else
+return 0;
+}
+int main()
+{
+int num;
+printf("Enter a number: ");
+scanf("%d", &num);
+if (palindrome(num))
+printf("%d is a palindrome number.", num);
+else
+printf("%d is not a palindrome number.", num);
+return 0;
+}
