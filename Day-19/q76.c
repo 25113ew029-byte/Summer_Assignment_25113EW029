f179958c81236ce9bd018060adf296ffe9e12a1d
Raunak Gupta <25113ew029@glbitm.ac.in>
2026-06-18 22:46:41 +0530

    Day-19 uploaded

---
+++ Day-19/q76.c
@@ -0,0 +1,19 @@
+// write a program to find diagonal sum.
+#include <stdio.h>
+
+int main() {
+    int a[3][3] = {
+        {1, 2, 3},
+        {4, 5, 6},
+        {7, 8, 9}
+    };
+    int i, sum = 0;
+
+    for(i = 0; i < 3; i++) {
+        sum = sum + a[i][i];
+    }
+
+    printf("Diagonal Sum = %d", sum);
+
+    return 0;
+}
